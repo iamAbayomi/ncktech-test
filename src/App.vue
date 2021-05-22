b8fa3d7894b0c6aca25c4f25ce172a03b957517e
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-05-22 04:37:01 +0100

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,60 @@
 <template>
-  <div id="app">
-      <div id ="menu-bar"> 
-          <div class="sub-menu">
-            TRIPS
-          </div>
-          <div class="sub-menu">
-            RECENTLY VIEWED
-          </div>
-          <div class="sub-menu">
-            BOOKINGS
-          </div>
-          <div class="sub-menu">
-            profile-image
-          </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
       </div>
 
+      <v-spacer></v-spacer>
 
-      <div id="main-details">
-       
-        <div class="menu-line"> ------ </div>
-       
-        <div id="payment-information-section">
-            <div class="card-type">
-              <p id="payment-title">
-                Payment Information
-              </p>
-              <p id="payment-subtitle">
-                Choose your method of payment
-              </p>
-              <img src="" alt="Credit Card image">
-            </div>
-            <div class="card-details">
-                <div id="payment-type">
-                    <img src="" alt="Visa">
-                    <img src="" alt="Discover">
-                    <img src="" alt="PayPal">
-                </div>
-                <div id="card-input-details">
-                    <form action="/card-details">
-                        <label for="credit-card-no">Credit Card Number</label>
-                        <input type="text" id="credit-card-no" name=""><br><br>
-                        <label for="expiration-date">Expiration Date</label>
-                        <input type="text" id="expiration-date" name=""><br><br>
-                        <label for="security-code">Security Code</label>
-                        <input type="text" id="security-code" name=""><br><br>
-                        <label for="postal-code">Postal Code</label>
-                        <input type="text" id="Postal Code" name=""><br><br>
-                        <p>Use this card for next time purchase </p>
-                        <input type="submit" value="Add Card">
-                    </form>
-                </div>
-            </div>
-        </div>
-        
-        <div class="menu-line"> ------ </div>
-        
-        <div id="payment-details-section">
-          <div id="sub-total-div">
-              <p id="subtotal-text"> 
-                Subtotal
-              </p>
-              <p id="subtotal-value"> 
-                #2,497.00
-              </p>
-          </div>
-          <div id="sub-total-div">
-              <p id="estimated-tax-text"> 
-                  Estimated TAX
-              </p>
-              <p id="estimated-tax-value"> 
-                  #119.64
-              </p>
-          </div>
-          <div id="sub-total-div">
-              <p id="promotional-code-text"> 
-                  Promotional Code
-              </p>
-              <p id="promotional-code-subttilte-text"> 
-                  :Z4KXLM94
-              </p>
-              <p id="promotional-code-value"> 
-                  #-60.00
-              </p>
-          </div>
-        </div>
-        
-        <div class="menu-line"> ------ </div>
-        
-        <div class="payment-accept-section"> 
-            <button class="blue-button"> Complete Payment</button>
-            <p id="payment-accept-button">TOTAL</p>
-            <p id="payment-accept-text">2556.64</p>
-        </div>
-      </div>
-      
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
 
-      
-
-  </div>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App'
-}
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-
-</style>
