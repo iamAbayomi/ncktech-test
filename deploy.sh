a59b202ee50e21b14accd116bc18c6d10931b43e
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-05-22 19:49:11 +0100

    changed the deploy path

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/ncktech-test
-git push -f git@github.com:iamabayomi/ncktech-test.git master:gh-pages
+git push -f git@github.com:iamabayomi/ncktech-test.git master
 
 cd -
